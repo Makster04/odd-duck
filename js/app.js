b6863f212b3dee0ac068838d23901109108b02a9
MAK TRNKA <maktrnka@comcast.net>
2024-02-06 09:22:18 -0800

    Changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // app.js
 
-// Constructor function for Product
+// CONSTRUCTOR FUNCTION for Product
 function Product(name, imagePath) {
     this.name = name;
     this.imagePath = imagePath;
@@ -9,10 +9,10 @@ function Product(name, imagePath) {
     Product.allProducts.push(this);
   }
   
-  // Static property to keep track of all products
+  // STATISTIC PROPERTY to keep track of all products
   Product.allProducts = [];
   
-  // Function to generate three unique random products
+  // FUNCTIONS TO GENERATE three unique random products
   function generateRandomProducts() {
     const uniqueProducts = [];
     while (uniqueProducts.length < 3) {
@@ -58,10 +58,10 @@ function Product(name, imagePath) {
   const totalRounds = 25;
   let currentRound = 1;
   
-// Event listener for the "View Results" button
+// EVENT LISTENER for the "View Results" button
 document.getElementById('view-results').addEventListener('click', displayResults);
 
-// Function to display voting results
+// FUNCTIONS TO DISPLAY voting results
 function displayResults() {
   const resultsContainer = document.getElementById('results-container');
   resultsContainer.innerHTML = ''; // Clear previous results
@@ -76,36 +76,36 @@ function displayResults() {
     resultsContainer.appendChild(resultElement);
   });
 
-  // Optionally, you can also hide the product container if you want
+  // OPTION TO HIDE PRODUCT CONTAINER
   document.getElementById('product-container').style.display = 'none';
   document.getElementById('view-results').style.display = 'none';
   resultsContainer.style.display = 'block';
 }
   
-  // Example products
-  new Product('Bat Bag', 'images/bag.jpg');
-  new Product('Bat Banana', 'images/banana.jpg');
-  new Product('Bat Bathroom', 'images/bathroom.jpg');
-  new Product('Bat Dog-Duck', 'images/dog-duck.jpg');
-  new Product('Bat Boots', 'images/boots.jpg');
-  new Product('Bat Breakfast', 'images/breakfast.jpg');
-  new Product('Bat Bubblegum', 'images/bubblegum.jpg');
-  new Product('Bat Chair', 'images/chair.jpg');
-  new Product('Bat Cthulhu', 'images/cthulhu.jpg');
-  new Product('Bat Dog-Duck', 'images/dog-duck.jpg');
-  new Product('Bat Dragon', 'images/dragon.jpg');
-  new Product('Bat Pen', 'images/pen.jpg');
-  new Product('Bat Pet-Sweep', 'images/pet-sweep.jpg');
-  new Product('Bat Scissors', 'images/scissors.jpg')
-  new Product('Bat Shark', 'images/shark.jpg');
-  new Product('Bat Sweep', 'images/sweep.png');
-  new Product('Bat Tauntaun', 'images/tauntaun.jpg');
-  new Product('Bat Unicorn', 'images/unicorn.jpg');
-  new Product('Bat Water-Can', 'images/water-can.jpg');
-  new Product('Bat Wine-Glass', 'images/wine-glass.jpg');
+  // PRODUCTS
+  new Product('Duck Bag', 'images/bag.jpg');
+  new Product('Duck Banana', 'images/banana.jpg');
+  new Product('Duck Bathroom', 'images/bathroom.jpg');
+  new Product('Duck Dog-Duck', 'images/dog-duck.jpg');
+  new Product('Duck Boots', 'images/boots.jpg');
+  new Product('Duck Breakfast', 'images/breakfast.jpg');
+  new Product('Duck Bubblegum', 'images/bubblegum.jpg');
+  new Product('Duck Chair', 'images/chair.jpg');
+  new Product('Duck Cthulhu', 'images/cthulhu.jpg');
+  new Product('Duck Dog-Duck', 'images/dog-duck.jpg');
+  new Product('Duck Dragon', 'images/dragon.jpg');
+  new Product('Duck Pen', 'images/pen.jpg');
+  new Product('Duck Pet-Sweep', 'images/pet-sweep.jpg');
+  new Product('Duck Scissors', 'images/scissors.jpg')
+  new Product('Duck Shark', 'images/shark.jpg');
+  new Product('Duck Sweep', 'images/sweep.png');
+  new Product('Duck Tauntaun', 'images/tauntaun.jpg');
+  new Product('Duck Unicorn', 'images/unicorn.jpg');
+  new Product('Duck Water-Can', 'images/water-can.jpg');
+  new Product('Duck Wine-Glass', 'images/wine-glass.jpg');
 
   
-  // Initial display of three random products
+  // INTIAL DISPLY of three random products
   const initialProducts = generateRandomProducts();
   initialProducts.forEach(product => {
     const imgElement = document.createElement('img');
